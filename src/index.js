7a6c0b5571d07775dee759b2c1aa570129b06cbf
Manny <coachmannytw@gmail.com>
2018-07-30 22:33:00 -0600

    graphql-yoga

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { GraphQLServer } = require('graphql-yoga')
+const fetch = require('node-fetch')
+
+
+const URL = `https://api.chucknorris.io/jokes/random`
+
+const typeDefs = `
+
+type Query {
+  getJoke: results
+}
+
+type results {
+  icon_url: String
+  value: String
+}
+`
+
+const resolvers = {
+    Query: {
+        getJoke: (parent) => {
+            return fetch(`${URL}`).then(res => res.json())
+        },
+    },
+
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
